5314f678363302aa8429c20313589b86681cc8c8
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-31 10:28:38 -0300

    feat: implementa um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "Qualifications") == 1651
+    assert count_ocurrences("data/jobs.csv", "Qualifications") != 0
+    assert count_ocurrences("data/jobs.csv", "testando") == 0
