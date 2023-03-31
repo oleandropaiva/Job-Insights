b164203c6c3ade08b91af0daeef6d5126596e4f7
Leandro Paiva <leandroapaiva@yahoo.com>
2023-03-31 16:12:08 -0300

    feat: implementa um teste para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    translate_dict_key = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert "title" in translate_dict_key[0]
+    assert "salary" in translate_dict_key[0]
+    assert "type" in translate_dict_key[0]
